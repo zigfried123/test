aae3b20c474bef54df02d50f81921e4ab61a8959
zigfried123 <zigfried123@mail.ru>
2025-05-22 14:28:30 +0400

    fix

--- client.php
+++ client.php
@@ -5,4 +5,4 @@ require_once __DIR__ . '/vendor/autoload.php';
 $client = new WebSocket\Client("ws://echo.websocket.org/");
 $client->text("Hello WebSocket.org!");
 echo $client->receive();
-$client->close();
+
